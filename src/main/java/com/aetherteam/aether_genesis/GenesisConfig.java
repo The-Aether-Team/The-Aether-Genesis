1fc20c27cf4f0a43bfda6914bd684544beb61420
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2023-05-22 20:03:36 -0700

    feat: Blue Aercloud Bounce Sound Effect

--- src/main/java/com/aetherteam/aether_genesis/GenesisConfig.java
+++ src/main/java/com/aetherteam/aether_genesis/GenesisConfig.java
@@ -6,6 +6,7 @@ import org.apache.commons.lang3.tuple.Pair;
 public class GenesisConfig {
     public static class Common {
         public final ForgeConfigSpec.ConfigValue<Boolean> gold_aercloud_ability;
+        public final ForgeConfigSpec.ConfigValue<Boolean> blue_aercloud_bounce_sfx;
 
         public Common(ForgeConfigSpec.Builder builder) {
             builder.push("Gameplay");
@@ -13,6 +14,10 @@ public class GenesisConfig {
                     .comment("Changes Gold Aercloud and respective parachute behavior to launch entities downwards")
                     .translation("config.aether_genesis.common.gameplay.gold_aercloud_ability")
                     .define("Gold Aerclouds launch entities downwards", true);
+            blue_aercloud_bounce_sfx = builder
+                    .comment("Makes Blue Aerclouds have their wobbly sounds that play when bouncing on them")
+                    .translation("config.aether_genesis.common.gameplay.blue_aercloud_bounce_sfx")
+                    .define("Blue Aercloud Bouncing Sounds", true);
             builder.pop();
         }
     }
