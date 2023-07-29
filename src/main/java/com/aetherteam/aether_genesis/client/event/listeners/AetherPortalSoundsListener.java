e9f7563e67d735a14813ba037e6cae520dbabcfb
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2023-07-29 18:38:35 -0400

    refactor(audio): attempt to use getSound()

--- src/main/java/com/aetherteam/aether_genesis/client/event/listeners/AetherPortalSoundsListener.java
+++ src/main/java/com/aetherteam/aether_genesis/client/event/listeners/AetherPortalSoundsListener.java
@@ -11,6 +11,8 @@ import net.minecraftforge.client.event.sound.PlaySoundEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
+import java.util.Random;
+
 @Mod.EventBusSubscriber
 public class AetherPortalSoundsListener {
 
@@ -18,9 +20,10 @@ public class AetherPortalSoundsListener {
     public static void alterSounds(PlaySoundEvent event)
     {
         SoundInstance newSound = null;
+        RandomSource random = RandomSource.create();
         SoundInstance oldSound = event.getOriginalSound();
         if (oldSound.getLocation() == AetherSoundEvents.BLOCK_AETHER_PORTAL_AMBIENT.get().getLocation()) {
-            newSound = new SimpleSoundInstance(GenesisSoundEvents.PORTAL_HUM.get(), oldSound.getSource(), oldSound.getVolume(), oldSound.getPitch(), RandomSource.create(), oldSound.getX(), oldSound.getY(), oldSound.getZ());
+            newSound = new SimpleSoundInstance(GenesisSoundEvents.PORTAL_HUM.get(), oldSound.getSource(), oldSound.getSound().getVolume().sample(random), oldSound.getSound().getPitch().sample(random), random, oldSound.getX(), oldSound.getY(), oldSound.getZ());
         }
         if (newSound != null) {
             event.setSound(newSound);
