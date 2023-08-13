1862277916c6edc3c5e194d6dd1de70e07647c9b
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2023-08-13 13:31:02 -0400

    fix: fixed ServerSoundUtilMixin, changing it to PortalSoundUtilMixin

--- src/main/java/com/aetherteam/aether_genesis/mixin/mixins/common/PortalSoundUtilMixin.java
+++ src/main/java/com/aetherteam/aether_genesis/mixin/mixins/common/PortalSoundUtilMixin.java
@@ -1,21 +1,15 @@
 package com.aetherteam.aether_genesis.mixin.mixins.common;
 
-import com.aetherteam.aether.util.ServerSoundUtil;
+import com.aetherteam.aether.block.portal.PortalSoundUtil;
 import com.aetherteam.aether_genesis.GenesisConfig;
 import com.aetherteam.aether_genesis.client.GenesisSoundEvents;
-import net.minecraft.core.BlockPos;
-import net.minecraft.sounds.SoundEvent;
-import net.minecraft.util.RandomSource;
-import net.minecraft.world.level.WorldGenLevel;
-import net.minecraft.world.level.chunk.ChunkGenerator;
-import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 import net.minecraftforge.registries.RegistryObject;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(ServerSoundUtil.class)
-public class ServerSoundUtilMixin {
+@Mixin(PortalSoundUtil.class)
+public class PortalSoundUtilMixin {
 
     @Redirect(remap = false, method = "playPortalSound", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/registries/RegistryObject;get()Ljava/lang/Object;"))
     private static Object replacePortalTravelSound(RegistryObject instance)
