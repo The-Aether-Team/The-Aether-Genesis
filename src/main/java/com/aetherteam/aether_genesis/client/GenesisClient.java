fb448ca74ca9a99f4a211f1c900ec6b9fd4d6be1
bconlon <bconlon.gml@gmail.com>
2023-08-12 22:36:12 -0700

    refactor: Replace Aether's menu toggle with Cumulus' menu switcher

--- src/main/java/com/aetherteam/aether_genesis/client/GenesisClient.java
+++ src/main/java/com/aetherteam/aether_genesis/client/GenesisClient.java
@@ -1,9 +1,12 @@
 package com.aetherteam.aether_genesis.client;
 
+import com.aetherteam.aether.AetherConfig;
 import com.aetherteam.aether_genesis.Genesis;
+import com.aetherteam.aether_genesis.GenesisConfig;
 import com.aetherteam.aether_genesis.blockentity.GenesisMenuTypes;
 import com.aetherteam.aether_genesis.blockentity.HolystoneFurnaceScreen;
 import com.aetherteam.aether_genesis.client.renderer.GenesisRenderers;
+import com.aetherteam.cumulus.CumulusConfig;
 import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -14,11 +17,21 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 public class GenesisClient {
     @SubscribeEvent
     public static void clientSetup(FMLClientSetupEvent event) {
+        setupMenuButtons();
         GenesisRenderers.registerCuriosRenderers();
         event.enqueueWork(() -> {
             registerGuiFactories();
             GenesisAtlases.registerSkyrootChestAtlases();
         });
+    }
+
+    public static void setupMenuButtons() {
+        if (GenesisConfig.CLIENT.genesis_menu_layout.get()) {
+            CumulusConfig.CLIENT.enable_menu_list_button.set(true);
+            CumulusConfig.CLIENT.enable_menu_list_button.save();
+            AetherConfig.CLIENT.enable_aether_menu_button.set(false);
+            AetherConfig.CLIENT.enable_aether_menu_button.save();
+        }
     }
 
     public static void registerGuiFactories() {
