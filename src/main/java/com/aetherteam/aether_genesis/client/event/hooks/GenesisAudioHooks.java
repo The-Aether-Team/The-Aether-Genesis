9c4a3cc45f7977ec28729a77a2d4ac8787b1e06f
bconlon <bconlon.gml@gmail.com>
2023-08-31 22:19:40 -0700

    improv: Prevent overlap of Aether II portal sounds

--- src/main/java/com/aetherteam/aether_genesis/client/event/hooks/GenesisAudioHooks.java
+++ src/main/java/com/aetherteam/aether_genesis/client/event/hooks/GenesisAudioHooks.java
@@ -1,17 +1,20 @@
 package com.aetherteam.aether_genesis.client.event.hooks;
 
+import com.aetherteam.aether.client.AetherSoundEvents;
 import com.aetherteam.aether_genesis.GenesisConfig;
 import com.aetherteam.aether_genesis.client.GenesisMusicManager;
 import com.aetherteam.aether_genesis.client.GenesisSoundEvents;
+import com.aetherteam.aether_genesis.mixin.mixins.client.accessor.SoundEngineAccessor;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.sounds.SimpleSoundInstance;
 import net.minecraft.client.resources.sounds.SoundInstance;
+import net.minecraft.client.sounds.SoundEngine;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundSource;
 
-public class GenesisAudioHooks {
-    public GenesisAudioHooks() {
-    }
+import java.util.List;
 
+public class GenesisAudioHooks {
     public static boolean shouldCancelMusic(SoundInstance sound) {
         if (sound.getSource() == SoundSource.MUSIC && GenesisConfig.CLIENT.night_music_tracks.get()) {
             return ((GenesisMusicManager.getSituationalMusic() != null && !sound.getLocation().equals(SimpleSoundInstance.forMusic(GenesisMusicManager.getSituationalMusic().getEvent().get()).getLocation())) && (GenesisMusicManager.getSituationalOppositeDaytimeMusic() != null && !sound.getLocation().equals(SimpleSoundInstance.forMusic(GenesisMusicManager.getSituationalOppositeDaytimeMusic().getEvent().get()).getLocation()))) || GenesisMusicManager.getCurrentMusic() != null && !sound.getLocation().equals(GenesisMusicManager.getCurrentMusic().getLocation());
@@ -26,6 +29,28 @@ public class GenesisAudioHooks {
         } else {
             return false;
         }
+    }
+
+    public static boolean shouldReplacePortalHum(SoundInstance sound) {
+        if (sound.getSource() == SoundSource.BLOCKS && GenesisConfig.COMMON.aether_ii_portal_sounds.get()) {
+            return sound.getLocation().equals(AetherSoundEvents.BLOCK_AETHER_PORTAL_AMBIENT.get().getLocation());
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean shouldCancelPortalSound(SoundEngine soundEngine, SoundInstance sound) {
+        if (sound != null) {
+            if (sound.getSource() == SoundSource.BLOCKS) {
+                if (sound.getLocation().equals(GenesisSoundEvents.PORTAL_HUM.get().getLocation())
+                        || sound.getLocation().equals(GenesisSoundEvents.PORTAL_TRAVEL.get().getLocation())
+                        || sound.getLocation().equals(GenesisSoundEvents.PORTAL_TRIGGER.get().getLocation())) {
+                    List<ResourceLocation> activeSounds = ((SoundEngineAccessor) soundEngine).genesis$getInstanceToChannel().keySet().stream().map(SoundInstance::getLocation).toList();
+                    return activeSounds.contains(sound.getLocation());
+                }
+            }
+        }
+        return false;
     }
 
     public static void tick() {
--- src/main/java/com/aetherteam/aether_genesis/client/event/listeners/GenesisAudioListener.java
+++ src/main/java/com/aetherteam/aether_genesis/client/event/listeners/GenesisAudioListener.java
@@ -1,7 +1,14 @@
 package com.aetherteam.aether_genesis.client.event.listeners;
 
+import com.aetherteam.aether_genesis.client.GenesisSoundEvents;
 import com.aetherteam.aether_genesis.client.event.hooks.GenesisAudioHooks;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.sounds.SimpleSoundInstance;
 import net.minecraft.client.resources.sounds.SoundInstance;
+import net.minecraft.client.sounds.SoundEngine;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.ClientPlayerNetworkEvent;
 import net.minecraftforge.client.event.sound.PlaySoundEvent;
@@ -10,19 +17,29 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 
-@Mod.EventBusSubscriber({Dist.CLIENT})
+@Mod.EventBusSubscriber(Dist.CLIENT)
 public class GenesisAudioListener {
-    public GenesisAudioListener() {
-    }
-
     @SubscribeEvent
     public static void onPlaySound(PlaySoundEvent event) {
+        SoundEngine soundEngine = event.getEngine();
         SoundInstance sound = event.getOriginalSound();
         if (GenesisAudioHooks.shouldCancelMusic(sound)) {
             event.setSound(null);
         }
-        if (GenesisAudioHooks.shouldCancelAercloudBounceSound(sound))
-        {
+        if (GenesisAudioHooks.shouldCancelAercloudBounceSound(sound)) {
+            event.setSound(null);
+        }
+
+        Level level = Minecraft.getInstance().level;
+        if (level != null) {
+            RandomSource random = level.getRandom();
+            if (GenesisAudioHooks.shouldReplacePortalHum(sound)) {
+                event.setSound(new SimpleSoundInstance(GenesisSoundEvents.PORTAL_HUM.get(), SoundSource.BLOCKS, 0.5F, random.nextFloat() * 0.4F + 0.8F, RandomSource.create(random.nextLong()), sound.getX(), sound.getY(), sound.getZ()));
+            }
+        }
+
+        SoundInstance newSound = event.getSound();
+        if (GenesisAudioHooks.shouldCancelPortalSound(soundEngine, newSound)) {
             event.setSound(null);
         }
     }
