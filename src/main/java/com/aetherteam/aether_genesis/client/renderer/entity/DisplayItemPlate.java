1cda3d3afc0086780033832f8a91054a09691761
bconlon <bconlon.gml@gmail.com>
2023-09-02 22:39:31 -0700

    improv: Finish Soaring Stone and Soaring Wisp

--- src/main/java/com/aetherteam/aether_genesis/client/renderer/entity/DisplayItemPlate.java
+++ src/main/java/com/aetherteam/aether_genesis/client/renderer/entity/DisplayItemPlate.java
@@ -4,11 +4,16 @@ import com.aetherteam.aether_genesis.entity.companion.Wisp;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
-import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.client.ForgeHooksClient;
+
+import javax.annotation.Nullable;
 
 public interface DisplayItemPlate {
     default void renderDisplayItem(Wisp entity, PoseStack poseStack, MultiBufferSource buffer, EntityRenderDispatcher entityRenderDispatcher, ItemRenderer itemRenderer) {
@@ -18,9 +23,20 @@ public interface DisplayItemPlate {
             poseStack.pushPose();
             poseStack.translate(0.0F, height, 0.0F);
             poseStack.mulPose(entityRenderDispatcher.cameraOrientation());
-            poseStack.translate(0.0F, -0.1, 0.0F);
+            poseStack.translate(0.0F, 0.3, 0.0F);
             poseStack.scale(0.5F, 0.5F, 0.1F);
-            itemRenderer.renderStatic(entity.getSummonItem(), ItemDisplayContext.HEAD, LightTexture.pack(15, 15), OverlayTexture.NO_OVERLAY, poseStack, buffer, entity.getLevel(), entity.getId()); //todo its still affected by light.
+            this.renderItem(itemRenderer, entity.getSummonItem(), LightTexture.FULL_BRIGHT, OverlayTexture.NO_OVERLAY, poseStack, buffer, entity.getLevel(), entity.getId());
+            poseStack.popPose();
+        }
+    }
+
+    default void renderItem(ItemRenderer itemRenderer, ItemStack itemStack, int combinedLight, int combinedOverlay, PoseStack poseStack, MultiBufferSource buffer, @Nullable Level level, int seed) {
+        if (!itemStack.isEmpty()) {
+            poseStack.pushPose();
+            poseStack.translate(-0.5F, -0.5F, -0.5F);
+            for (BakedModel model : itemRenderer.getModel(itemStack, level, null, seed).getRenderPasses(itemStack, true)) {
+                itemRenderer.renderModelLists(model, itemStack, combinedLight, combinedOverlay, poseStack, buffer.getBuffer(RenderType.cutout()));
+            }
             poseStack.popPose();
         }
     }
