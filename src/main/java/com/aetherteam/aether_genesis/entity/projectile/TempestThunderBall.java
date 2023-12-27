dfc9c466ea89a8cfd2819e2cb7fd7cbd488b5dea
bconlon <bconlon.gml@gmail.com>
2023-12-27 14:19:49 -0800

    refactor: More thunder projectile work

--- src/main/java/com/aetherteam/aether_genesis/entity/projectile/TempestThunderBall.java
+++ src/main/java/com/aetherteam/aether_genesis/entity/projectile/TempestThunderBall.java
@@ -1,27 +1,29 @@
 package com.aetherteam.aether_genesis.entity.projectile;
 
 import com.aetherteam.aether.capability.lightning.LightningTracker;
-import com.aetherteam.aether.entity.projectile.crystal.AbstractCrystal;
-import com.aetherteam.aether_genesis.Genesis;
 import com.aetherteam.aether_genesis.block.miscellaneous.ColdFireBlock;
 import com.aetherteam.aether_genesis.client.particle.GenesisParticleTypes;
 import com.aetherteam.aether_genesis.entity.GenesisEntityTypes;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.particles.DustParticleOptions;
-import net.minecraft.core.particles.ParticleOptions;
-import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientGamePacketListener;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LightningBolt;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.projectile.AbstractHurtingProjectile;
 import net.minecraft.world.level.GameRules;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.levelgen.Heightmap;
+import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.HitResult;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.network.NetworkHooks;
 
 
-public class TempestThunderBall extends AbstractCrystal {
+public class TempestThunderBall extends AbstractHurtingProjectile {
 	private int ticksInAir;
 
 	public TempestThunderBall(EntityType<? extends TempestThunderBall> entityType, Level level) {
@@ -43,19 +45,15 @@ public class TempestThunderBall extends AbstractCrystal {
 			level().addParticle(GenesisParticleTypes.TEMPEST_ELECTRICITY.get(), xOffset, yOffset, zOffset, 0.0, 0.0, 0.0);
 		}
 		super.tick();
+		Vec3 vector3d = this.getDeltaMovement();
+		double d2 = this.getX() + vector3d.x();
+		double d0 = this.getY() + vector3d.y();
+		double d1 = this.getZ() + vector3d.z();
+		this.updateRotation();
+		this.setPos(d2, d0, d1);
 	}
 
 	@Override
-	protected void tickMovement() {
-		super.tickMovement();
-		this.setDeltaMovement(this.getDeltaMovement().scale(0.99F));
-	}
-
-	@Override
-	protected ParticleOptions getExplosionParticle() {
-		return ParticleTypes.SMOKE;
-	}
-
 	protected void onHit(HitResult pResult) {
 		super.onHit(pResult);
 		if (!this.level().isClientSide()) {
@@ -71,21 +69,30 @@ public class TempestThunderBall extends AbstractCrystal {
 		}
 	}
 
+	@Override
+	protected void onHitEntity(EntityHitResult result) {
+		super.onHitEntity(result);
+		if (!this.level().isClientSide) {
+			Entity entity = result.getEntity();
+			Entity entity1 = this.getOwner();
+			entity.hurt(this.damageSources().indirectMagic(this, entity1), 4.0F);
+			if (entity1 instanceof LivingEntity) {
+				this.doEnchantDamageEffects((LivingEntity)entity1, entity);
+			}
+		}
+	}
+
 	private void spawnColdFire() {
 		if (!this.level().isClientSide() && this.level().getGameRules().getBoolean(GameRules.RULE_DOFIRETICK)) {
-			BlockPos blockPos = this.blockPosition();
-			BlockPos belowPos = blockPos.below(0);
+			BlockPos.MutableBlockPos mutablePos = this.blockPosition().mutable();
+			mutablePos.setY(this.level().getHeight(Heightmap.Types.OCEAN_FLOOR, mutablePos.getX(), mutablePos.getZ()));
+			BlockPos blockPos = mutablePos.immutable();
 			BlockState blockState = ColdFireBlock.getState(this.level(), blockPos);
-			for (int offset = 0; !this.level().getBlockState(belowPos).isAir() || !blockState.canSurvive(this.level(), belowPos); offset++) {
-				belowPos = blockPos.below(offset);
-			}
 
-			Genesis.LOGGER.info(belowPos + " " + blockState.canSurvive(this.level(), belowPos));
-
-			this.level().setBlockAndUpdate(belowPos, blockState);
+			this.level().setBlockAndUpdate(blockPos, blockState);
 
 			for (int i = 0; i < 4; ++i) {
-				BlockPos blockPos1 = belowPos.offset(this.random.nextInt(3) - 1, this.random.nextInt(3) - 1, this.random.nextInt(3) - 1);
+				BlockPos blockPos1 = blockPos.offset(this.random.nextInt(3) - 1, this.random.nextInt(3) - 1, this.random.nextInt(3) - 1);
 				blockState = ColdFireBlock.getState(this.level(), blockPos1);
 				if (this.level().getBlockState(blockPos1).isAir() && blockState.canSurvive(this.level(), blockPos1)) {
 					this.level().setBlockAndUpdate(blockPos1, blockState);
@@ -94,7 +101,21 @@ public class TempestThunderBall extends AbstractCrystal {
 		}
 	}
 
-	
+	@Override
+	public boolean isPickable() {
+		return false;
+	}
+
+	@Override
+	public boolean hurt(DamageSource source, float amount) {
+		return false;
+	}
+
+	@Override
+	protected boolean shouldBurn() {
+		return false;
+	}
+
 	@Override
 	public Packet<ClientGamePacketListener> getAddEntityPacket() {
 		return NetworkHooks.getEntitySpawningPacket(this);
