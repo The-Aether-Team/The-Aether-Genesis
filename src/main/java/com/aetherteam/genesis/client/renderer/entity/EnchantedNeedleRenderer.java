9105e2177014fbc0e8680534e06d1b04eef0c01b
bconlon <bconlon.gml@gmail.com>
2025-02-28 20:50:02 -0800

    improv: enchanted needle texture

--- src/main/java/com/aetherteam/genesis/client/renderer/entity/EnchantedNeedleRenderer.java
+++ src/main/java/com/aetherteam/genesis/client/renderer/entity/EnchantedNeedleRenderer.java
@@ -1,13 +1,13 @@
 package com.aetherteam.genesis.client.renderer.entity;
 
-import com.aetherteam.aether.Aether;
+import com.aetherteam.genesis.AetherGenesis;
 import com.aetherteam.genesis.entity.projectile.EnchantedNeedle;
 import net.minecraft.client.renderer.entity.ArrowRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 
 public class EnchantedNeedleRenderer extends ArrowRenderer<EnchantedNeedle> {
-    private static final ResourceLocation ENCHANTED_NEEDLE_TEXTURE = ResourceLocation.fromNamespaceAndPath(Aether.MODID, "textures/entity/projectile/dart/poison_needle.png"); //todo
+    private static final ResourceLocation ENCHANTED_NEEDLE_TEXTURE = ResourceLocation.fromNamespaceAndPath(AetherGenesis.MODID, "textures/entity/projectile/dart/enchanted_needle.png");
 
     public EnchantedNeedleRenderer(EntityRendererProvider.Context context) {
         super(context);
