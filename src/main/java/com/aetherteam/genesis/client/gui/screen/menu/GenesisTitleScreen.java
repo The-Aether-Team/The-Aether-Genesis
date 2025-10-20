bba36723c45838b2262d98e0bd43cedbfc5133c8
darksonic300 <98578683+darksonic300@users.noreply.github.com>
2025-10-20 15:49:31 +0200

    work: Genesis Menu ground work

---
+++ src/main/java/com/aetherteam/genesis/client/gui/screen/menu/GenesisTitleScreen.java
@@ -0,0 +1,6 @@
+package com.aetherteam.genesis.client.gui.screen.menu;
+
+import com.aetherteam.aether.client.gui.screen.menu.AetherTitleScreen;
+
+public class GenesisTitleScreen extends AetherTitleScreen {
+}
