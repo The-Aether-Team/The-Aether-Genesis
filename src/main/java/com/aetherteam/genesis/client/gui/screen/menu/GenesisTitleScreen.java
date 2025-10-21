32ff2ead0e109c3fb983fa0e1e7f71c4a44613ad
darksonic300 <98578683+darksonic300@users.noreply.github.com>
2025-10-21 09:26:44 +0200

    Revert "work: Genesis Menu ground work"
    This reverts commit 82ff2b6f793fd12ee2bb1eff2b66cc6b3115b90f.

--- src/main/java/com/aetherteam/genesis/client/gui/screen/menu/GenesisTitleScreen.java
+++
@@ -1,6 +0,0 @@
-package com.aetherteam.genesis.client.gui.screen.menu;
-
-import com.aetherteam.aether.client.gui.screen.menu.AetherTitleScreen;
-
-public class GenesisTitleScreen extends AetherTitleScreen {
-}
