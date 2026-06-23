564016cfee6eba8f4800a0f05f67143954c34615
a <EYCHFSJD@gmail.com>
2026-06-23 15:21:26 +0800

    fix a Carrion Sprout spawn bug.

--- src/main/java/com/aetherteam/genesis/entity/passive/CarrionSprout.java
+++ src/main/java/com/aetherteam/genesis/entity/passive/CarrionSprout.java
@@ -117,7 +117,7 @@ public class CarrionSprout extends Mob {
     @Override
     public void tick() {
         super.tick();
-        if (!this.level().getBlockState(this.blockPosition().below()).is(AetherTags.Blocks.AECHOR_PLANT_SPAWNABLE_ON) && !this.isPassenger()) {
+        if (!this.level().getBlockState(this.blockPosition().below()).is(GenesisTags.Blocks.CARRION_SPROUT_SPAWNABLE_ON) && !this.isPassenger()) {
             this.kill();
         }
         if (!this.level().isClientSide()) {
